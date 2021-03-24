c5cff019554cbadfe0245a7f9961069d536cd23c
Facundo Villalba <facundovillalba38@gmail.com>
2021-03-23 21:11:10 -0300

    initial structure

---
+++ src/main/java/com/utn/udeetpfinal/UdeeTpfinalApplication.java
@@ -0,0 +1,13 @@
+package com.utn.udeetpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeeTpfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeeTpfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/udeetpfinal/UdeeTpfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.udeetpfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeeTpfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
