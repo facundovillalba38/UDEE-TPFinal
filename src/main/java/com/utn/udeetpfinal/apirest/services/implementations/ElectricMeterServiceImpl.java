4962ad5e2059f8bbfdce404c626dd00c66b4915f
LeanTolo <leandrotoloza8@gmail.com>
2021-03-31 12:43:39 -0300

    repo,service,controller structure of User&Address

--- src/main/java/com/utn/udeetpfinal/apirest/services/implementations/ElectricMeterServiceImpl.java
+++ src/main/java/com/utn/udeetpfinal/apirest/services/implementations/ElectricMeterServiceImpl.java
@@ -3,5 +3,5 @@ package com.utn.udeetpfinal.apirest.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ElectricMeterService {
+public class ElectricMeterServiceImpl {
 }
