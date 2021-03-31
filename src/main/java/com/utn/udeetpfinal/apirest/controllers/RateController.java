172c7c24a1793e84a7d36cfb5167165a07ee8a4f
Facundo Villalba <facundovillalba38@gmail.com>
2021-03-31 12:43:25 -0300

    Electric Meter CRUD

--- src/main/java/com/utn/udeetpfinal/apirest/controllers/RateController.java
+++ src/main/java/com/utn/udeetpfinal/apirest/controllers/RateController.java
@@ -39,7 +39,4 @@ public class RateController {
         rateService.deleteRate(rateId);
     }
 
-
-
-
 }
--- src/main/java/com/utn/udeetpfinal/apirest/services/implementations/ElectricMeterServiceImpl.java
+++ src/main/java/com/utn/udeetpfinal/apirest/services/implementations/ElectricMeterServiceImpl.java
@@ -1,7 +1,40 @@
 package com.utn.udeetpfinal.apirest.services.implementations;
 
+import com.utn.udeetpfinal.apirest.models.ElectricMeter;
+import com.utn.udeetpfinal.apirest.repositories.IElectricMeterRepository;
+import com.utn.udeetpfinal.apirest.services.interfaces.IElectricMeterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ElectricMeterServiceImpl {
+public class ElectricMeterServiceImpl implements IElectricMeterService {
+
+    private final IElectricMeterRepository electricMeterRepository;
+
+    @Autowired
+    public ElectricMeterServiceImpl(IElectricMeterRepository electricMeterRepository) {
+        this.electricMeterRepository = electricMeterRepository;
+    }
+
+    @Override
+    public void addElectricMeter(ElectricMeter electricMeter) {
+        electricMeterRepository.save(electricMeter);
+    }
+
+    @Override
+    public List<ElectricMeter> getAll() {
+        return electricMeterRepository.findAll();
+    }
+
+    @Override
+    public ElectricMeter getElectricMeterById(Long electricMeterId) {
+        return electricMeterRepository.findById(electricMeterId).orElse(null);
+    }
+
+    @Override
+    public void deleteElectricMeter(Long electricMeterId) {
+        electricMeterRepository.deleteById(electricMeterId);
+    }
 }
